5ca854afc7945d459b143fba38a03f979e08b980
luchoster <luchoster@gmail.com>
2017-08-30 22:29:25 -0700

    Adding support for menus, to the theme. Also adding a screenshot for the theme.

--- functions.php
+++ functions.php
@@ -1,3 +1,8 @@
 <?php
   require_once ('config.php');
   require_once ('routes/index.php');
+
+  function add_menu_support() {
+        add_theme_support( 'menus'  );
+  }
+  add_action( 'after_setup_theme', 'add_menu_support'  );
