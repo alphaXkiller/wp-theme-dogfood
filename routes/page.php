b904fd5958143790ac24ac37012187f65ac6d04d
luchoster <luchoster@gmail.com>
2017-08-31 22:10:33 -0700

    Adding pages to the API

---
+++ routes/page.php
@@ -0,0 +1,40 @@
+<?php
+
+function _format_page_obj($raw_post) {
+    $post_id = $raw_post->ID;
+    $page = (object) [
+      'ID'             => $post_id,
+      'title'          => $raw_post->post_title,
+      'content'        => apply_filters('the_content', $raw_post->post_content),
+      'slug'           => $raw_post->post_name,
+      'featured_image' => get_the_post_thumbnail_url($post_id),
+    ];
+    return $page;
+}
+
+function getPageBySlug($request) {
+  $args = array(
+    'post_type' => 'page',
+    'name'      => $request['slug'],
+    'status'    => 'publish'
+  );
+
+  $query_result = new WP_Query($args);
+  if (is_null($query_result->post)) {
+    $err_code = 'Not Found';
+    $err_msg = 'page ('.$request['slug'].') not found.';
+    return new WP_Error($err_code, $err_msg, array('status' => 404));
+  } else {
+    return _format_page_obj($query_result->post);
+  }
+}
+
+function registerPageRoutes() {
+  register_rest_route(ENDPOINT_V1, '/page/(?P<slug>[a-zA-Z0-9-]+)', array(
+    'method' => 'GET',
+    'callback' => 'getPageBySlug'
+  ));
+}
+
+  add_action('rest_api_init', 'registerPageRoutes');
+?>
